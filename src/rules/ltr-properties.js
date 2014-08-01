a7ffef7d067a9ef8a1c056af9ef756b4d6445e14
Lewis Nyman <hello@lewisnyman.co.uk>
2014-08-01 13:36:11 +0200

    A rule that passes code standards

--- src/rules/ltr-properties.js
+++ src/rules/ltr-properties.js
@@ -1,40 +1,47 @@
 /*
- * Rule: Detects properties for LTR text styling the require equivilent RTL styling.
+ * Rule: Detects properties for LTR text styling the require equivalent RTL styling.
  */
 /*global CSSLint*/
 CSSLint.addRule({
+
     //rule information
     id: "ltr-properties",
     name: "LTR properties",
-    desc: "Detects properties for LTR text styling the require equivilent RTL styling.",
+    desc: "Detects properties for LTR text styling the require equivalent RTL styling.",
     browsers: "All",
 
     //initialization
     init: function(parser, reporter){
         "use strict";
-        var rule = this,
-            properties = {
-                "border": 1,
-                "border-left": 1,
-                "border-right": 1,
-                "border-radius": 1,
-                "border-top-right-radius": 1,
-                "border-top-left-radius": 1,
-                "border-bottom-right-radius": 1,
-                "border-bottom-left-radius": 1,
-                padding: 1,
-                "padding-left": 1,
-                "padding-right": 1,
-                margin: 1,
-                "margin-left": 1,
-                "margin-right": 1,
-                "text-align": 1,
-                float: 1,
-                clear: 1,
-                background: 1,
-                "background-position": 1,
-                left: 1,
-                right: 1
-            };
+        var properties = {
+            "border": 1,
+            "border-left": 1,
+            "border-right": 1,
+            "border-radius": 1,
+            "border-top-right-radius": 1,
+            "border-top-left-radius": 1,
+            "border-bottom-right-radius": 1,
+            "border-bottom-left-radius": 1,
+            padding: 1,
+            "padding-left": 1,
+            "padding-right": 1,
+            margin: 1,
+            "margin-left": 1,
+            "margin-right": 1,
+            "text-align": 1,
+            float: 1,
+            clear: 1,
+            background: 1,
+            "background-position": 1,
+            left: 1,
+            right: 1
+        };
+
+        parser.addListener("property", function(event){
+            var name = event.property.toString().toLowerCase();
+            if (properties[name]) {
+                reporter.report("Using " + name + "can require an equivalent rule for RTL styling.");
+            }
+        });
     }
 });
