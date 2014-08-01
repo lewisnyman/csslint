bd2ae06412e6fd32f965c430f0bebfea47a3cb5a
Lewis Nyman <hello@lewisnyman.co.uk>
2014-08-01 13:15:42 +0200

    Added list of LTR rules

--- src/rules/ltr-properties.js
+++ src/rules/ltr-properties.js
@@ -11,8 +11,30 @@ CSSLint.addRule({
 
     //initialization
     init: function(parser, reporter){
-        var rule = this;
-
-        //rule initialization
+        "use strict";
+        var rule = this,
+            properties = {
+                "border": 1,
+                "border-left": 1,
+                "border-right": 1,
+                "border-radius": 1,
+                "border-top-right-radius": 1,
+                "border-top-left-radius": 1,
+                "border-bottom-right-radius": 1,
+                "border-bottom-left-radius": 1,
+                padding: 1,
+                "padding-left": 1,
+                "padding-right": 1,
+                margin: 1,
+                "margin-left": 1,
+                "margin-right": 1,
+                "text-align": 1,
+                float: 1,
+                clear: 1,
+                background: 1,
+                "background-position": 1,
+                left: 1,
+                right: 1
+            };
     }
 });
